31cdfbdeda96ae3294a68919037cc3235a2b6af5
vilca942 <vilca942@hotmail.com>
2025-08-21 16:54:10 +0200

    pushing

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from src.models import Base
 
 
 def create_app() -> FastAPI:
-    """init fastapi app fake commit"""
+    """init fastapi app fake commit2"""
 
     app: FastAPI = FastAPI(
         title="database-load-tester",
